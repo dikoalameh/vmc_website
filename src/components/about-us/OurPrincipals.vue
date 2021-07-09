d4e2e12bec83fb1dbe1867a6d328e783ee1fe91f
ShanCaj <shantycajulao@gmail.com>
2021-07-09 12:37:30 +0800

    remove president name and lorem in principal page

--- src/components/about-us/OurPrincipals.vue
+++ src/components/about-us/OurPrincipals.vue
@@ -16,7 +16,7 @@
               650-0024, Japan —
               Tel: 078 334-9700 / Fax: 078-334-9719
             </p>
-            <p class="vmc-text-primary-2 mb-1">President : EIIJI KADONO</p>
+<!--            <p class="vmc-text-primary-2 mb-1">President : EIIJI KADONO</p>-->
             <p class="font-weight-light mb-0">
               The company was established as a shipowner in 1917 and has it’s headquarters in Kobe, Japan. Today the company’s main activities involve ship management. Together with the group companies KRBS-ROHQ in Philippines, Stargate Shipmanagement GmbH in Germany, Stargate Maritime Ltd. in Bulgaria and Stargate Ukraine, the KRBS group manages more than 100 vessels and over 2000 seafarers.            </p>
           </div>
@@ -35,10 +35,10 @@
               37-75, Shiomi-cho, Minato-ku, Nagoya, AICHI, 455-0028, Japan —
               TEL 81-52-614-5870 / FAX 81-52-614-5867
             </p>
-            <p class="vmc-text-primary-2 mb-1">President: CAPT. KAZUMI TOMI</p>
-            <p class="font-weight-light mb-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+<!--            <p class="vmc-text-primary-2 mb-1">President: CAPT. KAZUMI TOMI</p>-->
+<!--            <p class="font-weight-light mb-0">-->
+<!--              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+<!--            </p>-->
           </div>
         </div>
       </div>
@@ -55,10 +55,10 @@
               8 Shenton way, # 07-04 / 05 AXA Tower, Singapore 068811 —
               Tel: + 65-6333-8595 / Fax: + 65-6836-9258
             </p>
-            <p class="vmc-text-primary-2 mb-1">Chief Executive Officer: MAKOTO KATO</p>
-            <p class="font-weight-light mb-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+<!--            <p class="vmc-text-primary-2 mb-1">Chief Executive Officer: MAKOTO KATO</p>-->
+<!--            <p class="font-weight-light mb-0">-->
+<!--              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
+<!--            </p>-->
           </div>
         </div>
       </div>
@@ -76,7 +76,7 @@
               Nihonbashi-Kayabacho, Chuo-ku, Tokyo, Japan  <br/> —
               Tel: + 81 03 3663 5191 / Fax: +81 03 3663 5193
             </p>
-            <p class="vmc-text-primary-2 mb-1">President: KAORU MUKAI</p>
+<!--            <p class="vmc-text-primary-2 mb-1">President: KAORU MUKAI</p>-->
             <p class="font-weight-light mb-0">
               Sansho Kaiun - SANSHO KAIUN COMPANY, LTD. Manages operation of ocean going chemical tankers and oil tankers, under the following basic policy.
               <br/>
