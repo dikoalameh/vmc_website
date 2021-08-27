71506e942162d1923af2e1b6f48f60707a1d661d
ShanCaj <shantycajulao@gmail.com>
2021-08-27 22:53:07 +0800

    add hiring post for master, cm, oiler

--- src/components/career/CareersInVMC.vue
+++ src/components/career/CareersInVMC.vue
@@ -7,6 +7,11 @@
             <h2 class="h1 vmc-text-primary vmc-heading">
               Veritas Careers
             </h2>
+            <h4 class="font-weight-normal vmc-text-primary-2 mt-3 text-left">MASTER MARINER:</h4>
+            <ol class="text-left">
+              <li class="pl-2">Holder of valid Master Mariner COC/COE and other documents required as per STCW</li>
+              <li class="pl-2">With Cape size vessel experience as Master for 18 months.</li>
+            </ol>
           </div>
         </div>
       </div>
@@ -15,7 +20,24 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <p>Content coming soon . . .</p>
+            <h4 class="font-weight-normal vmc-text-primary-2">CHIEF MATE (CHIEF OFFICER):</h4>
+            <ol>
+              <li class="pl-2">Holder of valid Chief Mate COC/COE and other documents required as per STCW</li>
+              <li class="pl-2">With Cape size (or Panamax) vessel experience as C/M for 18 months</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid vmc-py-big">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h4 class="font-weight-normal vmc-text-primary-2">FITTER OR NO. 1 OILER:</h4>
+            <ol>
+              <li class="pl-2">Certified welder with valid documents required as per SCTW</li>
+              <li class="pl-2">With at least 18 months experience as Fitter or No.1 Oiler onboard worldwide vessels.</li>
+            </ol>
           </div>
         </div>
       </div>
