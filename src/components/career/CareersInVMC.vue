127baf78ec05e8dd1108333c7a30113766f67912
Your Name <you@example.com>
2025-05-04 03:32:52 +0800

    feat(careers): update careers page 1 from office and one crew hiring

--- src/components/career/CareersInVMC.vue
+++ src/components/career/CareersInVMC.vue
@@ -15,86 +15,10 @@
       <div class="container">
         <div class="row">
           <div class="col-md-6 my-2">
-            <div class="card">
-              <div class="card-body">
-                <h3>1. Executive Driver</h3>
-                <h5>Qualifications:</h5>
-                <ul>
-                  <li>Candidate must possess at least a High School Diploma.</li>
-                  <li>Candidate must possess a professional driver's license.</li>
-                  <li>At least 2 year(s) of working experience in the related field is required for this position.</li>
-                  <li>Knowledgeable in basic troubleshooting</li>
-                  <li>Knowledgeable in Manila/Makati routes</li>
-                  <li>Can drive manual and automatic transmission SUV vehicles</li>
-                  <li>Applicants must be willing to work in Makati City.</li>
-                </ul>
-              </div>
-            </div>
+            <img :src="section1" alt="" class="border" style="height: auto; width: 100%;">
           </div>
           <div class="col-md-6 my-2">
-            <div class="card">
-              <div class="card-body">
-                <h3>2. Executive Assistant</h3>
-                <h5>Qualifications:</h5>
-                <ul>
-                  <li>Candidate must possess at least a Bachelor's/College Degree, any field.</li>
-                  <li>At least 3 year(s) of working experience in the related field is required for this position.</li>
-                  <li>Excellent communication skills.</li>
-                  <li>Ability to function well in a high-paced environment</li>
-                  <li>Highly proficient in MS Office applications, especially Excel, Word, PowerPoint and Outlook</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 my-2">
-            <div class="card">
-              <div class="card-body">
-                <h3>3. Accounting/Finance Staff</h3>
-                <h5>Qualifications:</h5>
-                <ul>
-                  <li>Bachelor's Degree in Accountancy, or any Finance-related courses.</li>
-                  <li>With knowledge in General / Cost Accounting / Financial Accounting or Basic Accounting</li>
-                  <li>Excellent communication skills.</li>
-                  <li>Ability to function well in a high-paced environment</li>
-                  <li>Highly proficient in MS Office applications, especially Excel, Word, and Outlook</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 my-2">
-            <div class="card">
-              <div class="card-body">
-                <h3>4. Documentation Assistant</h3>
-                <h5>Qualifications:</h5>
-                <ul>
-                  <li>Candidate must possess at least a Bachelor's/College Degree, any field.</li>
-                  <li>Excellent communication skills.</li>
-                  <li>Ability to function well in a high-paced environment</li>
-                  <li>Highly proficient in MS Office applications, especially Excel, Word, and Outlook</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 my-2">
-            <div class="card">
-              <div class="card-body">
-                <h3>5. Training Assistant</h3>
-                <h5>Qualifications:</h5>
-                <ul>
-                  <li>Candidate must possess at least a Bachelor's/College Degree, any field.</li>
-                  <li>Excellent communication skills.</li>
-                  <li>Ability to function well in a high-paced environment</li>
-                  <li>Highly proficient in MS Office applications, especially Excel, Word, and Outlook</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12 my-4">
-            <h4 class="vmc-text-primary text-center mb-0">
-            Kindly send your resume to <a href="mailto:bea.aquino@veritas.com.ph">bea.aquino@veritas.com.ph</a>
-            </h4>
+            <img :src="section2" alt="" class="border" style="height: auto; width: 100%;">
           </div>
         </div>
       </div>
@@ -104,7 +28,14 @@
 
 <script>
 export default {
-name: "CareersInVMC"
+  name: "CareersInVMC",
+  data(){
+    return {
+      section1 : require(`@/assets/careers/2025/vmcHiring_04222025.jpg`),
+      section2 : require(`@/assets/careers/2025/vmcHiring_03062025.jpg`),
+    }
+  }
+
 }
 </script>
 
