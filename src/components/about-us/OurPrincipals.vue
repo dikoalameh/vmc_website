33eab6ffebf0c062451899c8fefddd6d6b16e531
Your Name <you@example.com>
2025-05-04 15:56:29 +0800

    feat(principals): update label and address of principals

--- src/components/about-us/OurPrincipals.vue
+++ src/components/about-us/OurPrincipals.vue
@@ -34,10 +34,6 @@
             <p class="mb-1 small">
               5F, Miyuki Bldg, 3-20-27, Nishiki, Naka-ku, Nagoya, Aichi, Japan
             </p>
-<!--            <p class="vmc-text-primary-2 mb-1">President: CAPT. KAZUMI TOMI</p>-->
-<!--            <p class="font-weight-light mb-0">-->
-<!--              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.-->
-<!--            </p>-->
           </div>
         </div>
       </div>
@@ -49,7 +45,7 @@
             <img :src="ftlogistics" alt="">
           </div>
           <div class="col-lg-8 my-3">
-            <h4 class="font-weight-normal vmc-text-primary-2 mb-0">FT LOGISTICS (ASIA) PTE. LTD. – Affiliate of Fujitrans</h4>
+            <h4 class="font-weight-normal vmc-text-primary-2 mb-0">FT LOGISTICS (ASIA) PTE. LTD.</h4>
             <p class="small mb-1">
               8 Shenton way, # 07-04 / 05 AXA Tower, Singapore 068811 —
               Tel: + 65-6333-8595 / Fax: + 65-6836-9258
@@ -72,7 +68,7 @@
           <div class="col-lg-12 order-lg-0 my-3">
             <h4 class="font-weight-normal mb-0 vmc-text-primary-2">SANSHO KAIUN CO., LTD. </h4>
             <p class="mb-1 small">
-              2f Hikari Bldg, 3-10-3 Chome  Nihonbashi-Kabayacho Chuo-ku, Tokyo Japan
+              9F, Kayabacho 2-Chome Bldg, 2-4-5, Nihonbashi Kayabacho, Chuo-Ku, Tokyo, Japan
               <br/> —
               Tel: + 81 03 3663 5191 / Fax: +81 03 3663 5193
             </p>
